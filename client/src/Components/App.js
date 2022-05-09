ae2b811555a6057b7bc84877321d9b37d026e7f0
Read Langworthy <rlangworthy@gmail.com>
2022-05-09 12:30:00 -0700

    Starting React

---
+++ client/src/Components/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p> Welcome to React. </p>
+        
+        Learn React
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
